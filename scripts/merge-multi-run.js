a45f5654927f17c6032a970cbc1db391a3490085
Patrick Hulce <patrick.hulce@gmail.com>
2017-12-20 15:14:06 -0800

    feat(scripts): add multi-run merge script

---
+++ scripts/merge-multi-run.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const _ = require('lodash')
+const fs = require('fs')
+const path = require('path')
+const METRIC_NAMES = require('../lib/analyzers/analyzer').METRIC_NAMES
+
+const RUN_A = process.argv[2]
+const RUN_B = process.argv[3]
+const fileContentA = JSON.parse(fs.readFileSync(RUN_A))
+const fileContentB = JSON.parse(fs.readFileSync(RUN_B))
+
+const siteData = new Map()
+function indexSiteData(sites, key, metrics) {
+  for (const site of sites) {
+    const data = siteData.get(site.url) || {}
+    for (const [dstName, srcName] of metrics.entries()) {
+      data[`${key}.${dstName}`] = site[srcName].median
+    }
+    siteData.set(site.url, data)
+  }
+}
+
+const defaultMetrics = new Map(METRIC_NAMES.map(x => [x, x]))
+const lanternMetrics = new Map([
+  ['firstContentfulPaint', 'roughEstimateOfFCP'],
+  ['firstMeaningfulPaint', 'roughEstimateOfFMP'],
+  ['timeToConsistentlyInteractive', 'roughEstimateOfTTCI'],
+])
+
+indexSiteData(fileContentA.sites, 'runA', defaultMetrics)
+indexSiteData(fileContentB.sites, 'runB', defaultMetrics)
+
+const collatedOutput = Array.from(siteData.entries())
+  .map(item => ({url: item[0], ...item[1]}))
+  .filter(item => item['runA.firstContentfulPaint'] && item['runB.firstContentfulPaint'])
+console.log(JSON.stringify(collatedOutput, null, 2))
