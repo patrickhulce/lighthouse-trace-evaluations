3e53a3a11ff9f560f885092bc64e97999cd3367a
Patrick Hulce <patrick.hulce@gmail.com>
2018-02-15 09:45:26 -0800

    fix(scripts): friendlier msg

--- scripts/eval-lantern-hashes.sh
+++ scripts/eval-lantern-hashes.sh
@@ -6,7 +6,7 @@ LH_DIRECTORY=../lighthouse
 CURRENT_HASH=$(cd $LH_DIRECTORY && git rev-parse HEAD)
 CURRENT_POS=$(cd $LH_DIRECTORY && git name-rev $CURRENT_HASH | cut -d " " -f 2)
 
-echo "WILL GO BACK TO $CURRENT_POS"
+echo "Bookmark made. Will go back to $CURRENT_POS when finished"
 for githash in ${HASHES[*]}; do
   echo "Checking out and evaluating $githash..."
   ( cd $LH_DIRECTORY && git checkout -f $githash )
